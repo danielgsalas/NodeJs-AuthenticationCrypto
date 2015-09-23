5221220810e7c1517554c23be28863b0344dc9a4
danielgsalas <danielgsalas@yahoo.com>
2015-09-23 05:37:39 -0700

    added data model for user

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+// app/models/user.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var UserSchema   = new Schema({
+    username : String,
+    password : String
+});
+
+module.exports = mongoose.model('User', UserSchema);
