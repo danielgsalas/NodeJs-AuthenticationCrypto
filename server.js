c965e1715ff962b9f890677726063c3b0f0b952f
danielgsalas <danielgsalas@yahoo.com>
2015-09-24 05:46:45 -0700

    added API functions: randomChars and (stubbed) fulltest

--- server.js
+++ server.js
@@ -19,14 +19,76 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// more routes for our API will happen here
+//middleware to use for all requests
+router.use(function(req, res, next) {
+	
+	// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    
+	// do logging
+    console.log("Received request: " + req.url);
+    next(); // make sure we go to the next routes and don't stop here
+});
 
+/*
+ * Automated integration test.
+ * 
+ * Example: http://localhost:8080/api/fulltest
+ */
+router.route('/fulltest')
+
+	.get(function(req, res) {
+	
+		/*
+		 * 
+		 * Test user preparation:
+		 * 1. create salt
+		 * 2. create username
+		 * 3. save username and salt
+		 * 4. create password (mock email)
+		 * 5. change password
+		 * 
+		 * Test sign in:
+		 * 1. get salt for user name
+		 * 3. submit username and salted password
+		 */
+	
+		res.status(401).send("Test not complete");
+	});
+
+// Create a string of random digits and letters (upper and lower case).
+// Example: http://localhost:8080/api/randomChars/6
+router.route("/randomChars/:length")
+
+	.get(function(req, res) {
+		if (req.params.length < 1) {
+			// 400 Bad Request
+			res.status(400).send("Length must be between 1 and 256 characters");
+		}
+		else if (req.params.length > 256) {
+			// 400 Bad Request
+	    	res.status(400).send("Length must be < 256 characters, otherwise unpredictability will be broken");
+		}
+		else {			
+			var authCrypto = require("./app/crypto/authenticationCrypto");
+			var targetLength = parseInt(req.params.length);
+			var randomChars = authCrypto.getRandomChars(targetLength);
+
+			res.json({ 
+				randomChars: randomChars
+			});
+		}		
+    });
+	
+/*
+ * Create, read, update or delete a user.
+ */
 router.route('/user')
 
 	/*
 	 * Create a user
 	 * 
-	 * Test procedure:
+	 * Manual test procedure:
 	 * 1. Run MondoDB server
 	 * 2. Run Node.js server
 	 * 3. Run Postman
